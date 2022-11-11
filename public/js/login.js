11ce6d8b05c484c86c7e45bd913a0f50366dfa19
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2022-11-11 09:42:02 -0800

    push for askbcs

--- public/js/login.js
+++ public/js/login.js
@@ -1,46 +1,47 @@
 const loginForm = document.querySelector(".login");
-loginForm.addEventListener("submit",e=>{
-    e.preventDefault();
-    console.log('PREVENTED DEFAULT!')
-    const userObj = {
-        email:document.querySelector("#loginEmail").value,
-        password:document.querySelector("#loginPassword").value
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("PREVENTED DEFAULT!");
+  const userObj = {
+    email: document.querySelector("#loginEmail").value,
+    password: document.querySelector("#loginPassword").value,
+  };
+  fetch("/api/users/login", {
+    method: "POST",
+    body: JSON.stringify(userObj),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    if (res.ok) {
+      location.reload();
+    } else {
+      alert("trumpet sound");
     }
-    fetch("/api/users/login",{
-        method:"POST",
-        body:JSON.stringify(userObj),
-        headers:{
-            "Content-Type":"application/json"
-        }
-    }).then(res=>{
-        if(res.ok){
-           location.reload()
-        } else {
-            alert("trumpet sound")
-        }
-    })
-})
+  });
+});
 
 const signupForm = document.querySelector(".signup");
-signupForm.addEventListener("submit",e=>{
-    e.preventDefault();
-    console.log('PREVENTED DEFAULT!')
-    const userObj = {
-        email:document.querySelector("#signupEmail").value,
-        name:document.querySelector("#signupName").value,
-        password:document.querySelector("#signupPassword").value
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("PREVENTED DEFAULT!");
+  const userObj = {
+    email: document.querySelector(".signupEmail").value,
+    name: document.querySelector(".signupName").value,
+    password: document.querySelector(".signupPassword").value,
+  };
+  // console.log(userObj)
+  fetch("/api/users/", {
+    method: "POST",
+    body: JSON.stringify(userObj),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    if (res.ok) {
+      location.reload();
+    } else {
+      alert("trumpet sound");
     }
-    fetch("/api/users/",{
-        method:"POST",
-        body:JSON.stringify(userObj),
-        headers:{
-            "Content-Type":"application/json"
-        }
-    }).then(res=>{
-        if(res.ok){
-           location.reload()
-        } else {
-            alert("trumpet sound")
-        }
-    })
-})
+  });
+});
